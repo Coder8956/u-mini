4b4c3cc326f2a923c2dd4fc99a1188a0f6b5f890
yanhao.home <yanhao.violet@gmail.com>
2024-07-04 21:54:49 +0800

    [modify] 重写对象池

--- ProjectUMini/Assets/UMiniFramework/Scripts/Function/Audio/EffectFunc.cs
+++ ProjectUMini/Assets/UMiniFramework/Scripts/Function/Audio/EffectFunc.cs
@@ -7,19 +7,10 @@ namespace UMiniFramework.Scripts.Function.Audio
     public class EffectFunc : AudioFunc
     {
         private AudioSource m_originalEffect;
-        private GameObjectPool m_effectPool;
 
         public override void Init()
         {
             m_originalEffect = CreateOriginalAudio();
-            m_effectPool = CreateEffectAudioPool(m_originalEffect.gameObject, 5);
-        }
-
-        private GameObjectPool CreateEffectAudioPool(GameObject original, uint initCount)
-        {
-            GameObjectPool goPool = PoolTool.CreateGameObjectPool("AudioEffectPool", gameObject);
-            goPool.Init(original, initCount);
-            return goPool;
         }
 
         private AudioSource CreateOriginalAudio()
