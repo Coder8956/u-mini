7903c8fec8207bb273e1271753016af61ae98e94
yanhao.notebook <yanhao.fang@qq.com>
2024-07-05 22:12:11 +0800

    [modify] 修改对象池编码

--- ProjectUMini/Assets/UMiniFramework/Scripts/Pool/GameObjectPool.cs
+++ ProjectUMini/Assets/UMiniFramework/Scripts/Pool/GameObjectPool.cs
@@ -1,13 +1,23 @@
-﻿using UMiniFramework.Scripts.Kit;
+﻿using System.Collections.Generic;
+using UMiniFramework.Scripts.Kit;
 using UnityEngine;
 
 namespace UMiniFramework.Scripts.Pool
 {
-    public class GameObjectPool : MonoBehaviour, IPool<GameObject>
+    public class GameObjectPool : MonoBehaviour
     {
+        private Queue<GameObject> m_gameObject;
+
         public static GameObjectPool CreatePool(string poolName, GameObject parent = null)
         {
-            return UMTool.CreateGameObject<GameObjectPool>(poolName, parent);
+            GameObjectPool newGameObjectPool = UMTool.CreateGameObject<GameObjectPool>(poolName, parent);
+            newGameObjectPool.Init();
+            return newGameObjectPool;
+        }
+
+        private void Init()
+        {
+            m_gameObject = new Queue<GameObject>();
         }
 
         public GameObject Get()
