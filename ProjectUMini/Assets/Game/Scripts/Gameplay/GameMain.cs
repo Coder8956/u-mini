db39addf5b186881a2db411dbeebfef902fbc681
yanhao.notebook <yanhao.fang@qq.com>
2024-07-16 21:52:31 +0800

    [modify] 开始处理炮台水平旋转

--- ProjectUMini/Assets/Game/Scripts/Gameplay/GameMain.cs
+++ ProjectUMini/Assets/Game/Scripts/Gameplay/GameMain.cs
@@ -8,6 +8,8 @@ namespace Game.Scripts.Gameplay
 {
     public class GameMain : MonoBehaviour
     {
+        [SerializeField] private Camera m_gameCamera;
+        [SerializeField] private GameObject m_cannon;
         private static string GameLevelId = string.Empty;
         private LevelData m_levelData;
         private GameAudioData m_gameAudioData;
@@ -24,6 +26,30 @@ namespace Game.Scripts.Gameplay
             m_levelData = UMini.Config.GetTable<LevelTable>().GetDataById(GameLevelId);
             m_gameAudioData = UMini.Config.GetTable<GameAudioTable>().GetDataById(m_levelData.bgmId);
             UMini.Audio.BGM.Play(m_gameAudioData.path, m_gameAudioData.volume, m_gameAudioData.loop);
+        }
+
+        void Update()
+        {
+            Vector3 mouseWorldPos = GetMousePosInWorld();
+// 计算方向
+            Vector3 direction = mouseWorldPos - transform.position;
+
+            // 计算旋转
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            
+            // 设置旋转
+            m_cannon.transform.rotation = rotation;
+            if (Input.GetMouseButtonDown(0))
+            {
+            }
+        }
+
+        private Vector3 GetMousePosInWorld()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10; // 深度，具体值根据你的场景而定
+            Vector3 worldPosition = m_gameCamera.ScreenToWorldPoint(mousePosition);
+            return worldPosition;
         }
     }
 }
