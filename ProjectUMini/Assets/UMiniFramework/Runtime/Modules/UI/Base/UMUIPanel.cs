dd29168e1ea141a6398a21bb806a7bfd4ffe32c2
yanhao.home <yanhao.home@gmail.com>
2025-02-20 19:37:21 +0800

    [modify]  更新 UI模块

--- ProjectUMini/Assets/UMiniFramework/Runtime/Modules/UI/Base/UMUIPanel.cs
+++ ProjectUMini/Assets/UMiniFramework/Runtime/Modules/UI/Base/UMUIPanel.cs
@@ -6,9 +6,24 @@ namespace UMiniFramework.Runtime.Modules.UI.Base
 {
     public abstract class UMUIPanel : MonoBehaviour
     {
+        /// <summary>
+        /// 在创建的时候执行一次
+        /// </summary>
         protected abstract void OnCreatePanel();
+        
+        /// <summary>
+        /// 在销毁的时候执行一次
+        /// </summary>
         protected abstract void OnDestroyPanel();
+        
+        /// <summary>
+        /// 每次打开的时候执行
+        /// </summary>
         protected abstract void OnOpenPanel();
+        
+        /// <summary>
+        /// 每次关闭的时候执行
+        /// </summary>
         protected abstract void OnClosePanel();
     }
 }
