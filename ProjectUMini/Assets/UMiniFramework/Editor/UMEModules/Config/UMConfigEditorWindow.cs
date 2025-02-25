355e6e52479e96ca1e260c20e696d4e147d62db0
yanhao.home <yanhao.home@gmail.com>
2025-02-25 17:55:54 +0800

    [modify]  开始处理配置模块编辑器界面

--- ProjectUMini/Assets/UMiniFramework/Editor/UMEModules/Config/UMConfigEditorWindow.cs
+++ ProjectUMini/Assets/UMiniFramework/Editor/UMEModules/Config/UMConfigEditorWindow.cs
@@ -5,6 +5,11 @@ namespace UMiniFramework.Editor.UMEModules.Config
 {
     public class UMConfigEditorWindow : EditorWindow
     {
+        private const string GUI_STYLE_HELPBOX = "HelpBox";
+        private string m_configInputDir = string.Empty;
+        private string m_configScriptOutputDir = string.Empty;
+        private string m_configJsonOutputDir = string.Empty;
+
         [MenuItem("UMUtils/Config/Config-Window")]
         private static void ShowWindow()
         {
@@ -19,8 +24,7 @@ namespace UMiniFramework.Editor.UMEModules.Config
 
             int layoutHeight = 20;
 
-            // GUILayout.Label("Select UIPanel Prefab Folder:", EditorStyles.boldLabel);
-            GUILayout.Label("UI Prefab Root Folder", GUILayout.Width(125), GUILayout.Height(layoutHeight));
+            GUILayout.Label("Config Input Directory", GUILayout.Width(130), GUILayout.Height(layoutHeight));
 
             if (GUILayout.Button("Select", GUILayout.Width(50), GUILayout.Height(layoutHeight)))
             {
@@ -29,32 +33,86 @@ namespace UMiniFramework.Editor.UMEModules.Config
 
                 if (!string.IsNullOrEmpty(selectedPath))
                 {
-                    if (!selectedPath.Contains(Application.dataPath))
-                    {
-                        EditorUtility.DisplayDialog("Tip",
-                            $"The selected folder is invalid. Please select a folder inside path {Application.dataPath}",
-                            "OK");
-                    }
-                    else
-                    {
-                    }
+                    m_configInputDir = selectedPath;
                 }
             }
 
             if (GUILayout.Button("Clear", GUILayout.Width(50), GUILayout.Height(layoutHeight)))
             {
                 // 清除路径
+                m_configInputDir = string.Empty;
             }
 
-            // GUILayout.Label("Selected Folder Path: ", GUI_STYLE_HELPBOX, GUILayout.Height(layoutHeight));
-            // GUILayout.Label(m_panelPrefabRootFolder, GUI_STYLE_HELPBOX, GUILayout.Height(layoutHeight));
+            GUILayout.Label(m_configInputDir, GUI_STYLE_HELPBOX, GUILayout.Height(layoutHeight));
 
             EditorGUILayout.EndHorizontal();
         }
+        
+        private void DrawSelectConfigScriptOutputFolder()
+        {
+            EditorGUILayout.BeginHorizontal();
 
+            int layoutHeight = 20;
+
+            GUILayout.Label("Config Script Output Directory", GUILayout.Width(180), GUILayout.Height(layoutHeight));
+
+            if (GUILayout.Button("Select", GUILayout.Width(50), GUILayout.Height(layoutHeight)))
+            {
+                // 打开文件夹选择框
+                string selectedPath = EditorUtility.OpenFolderPanel("Select Folder", Application.dataPath, "");
+
+                if (!string.IsNullOrEmpty(selectedPath))
+                {
+                    m_configScriptOutputDir = selectedPath;
+                }
+            }
+
+            if (GUILayout.Button("Clear", GUILayout.Width(50), GUILayout.Height(layoutHeight)))
+            {
+                // 清除路径
+                m_configScriptOutputDir = string.Empty;
+            }
+
+            GUILayout.Label(m_configScriptOutputDir, GUI_STYLE_HELPBOX, GUILayout.Height(layoutHeight));
+
+            EditorGUILayout.EndHorizontal();
+        }
+        
+        private void DrawSelectConfigJsonOutputFolder()
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            int layoutHeight = 20;
+
+            GUILayout.Label("Config Json Output Directory", GUILayout.Width(180), GUILayout.Height(layoutHeight));
+
+            if (GUILayout.Button("Select", GUILayout.Width(50), GUILayout.Height(layoutHeight)))
+            {
+                // 打开文件夹选择框
+                string selectedPath = EditorUtility.OpenFolderPanel("Select Folder", Application.dataPath, "");
+
+                if (!string.IsNullOrEmpty(selectedPath))
+                {
+                    m_configJsonOutputDir = selectedPath;
+                }
+            }
+
+            if (GUILayout.Button("Clear", GUILayout.Width(50), GUILayout.Height(layoutHeight)))
+            {
+                // 清除路径
+                m_configJsonOutputDir = string.Empty;
+            }
+
+            GUILayout.Label(m_configJsonOutputDir, GUI_STYLE_HELPBOX, GUILayout.Height(layoutHeight));
+
+            EditorGUILayout.EndHorizontal();
+        }
+        
         private void OnGUI()
         {
             DrawSelectConfigInputFolder();
+            DrawSelectConfigScriptOutputFolder();
+            DrawSelectConfigJsonOutputFolder();
         }
     }
 }
