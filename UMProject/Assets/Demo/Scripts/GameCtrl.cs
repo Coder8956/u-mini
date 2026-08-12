c5717cfc2a7664b514019fd125833f9ed2bfb520
yanhao.laptop <yanhao.laptop@gmail.com>
2026-08-12 10:29:12 +0800

    [modify] update.

---
+++ UMProject/Assets/Demo/Scripts/GameCtrl.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameCtrl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
