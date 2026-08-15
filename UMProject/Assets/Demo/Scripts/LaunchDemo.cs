257992348fb40a396d425fe2be8f3da3c20bad99
yanhao.laptop <yanhao.laptop@gmail.com>
2026-08-16 00:07:40 +0800

    [modify] update.

--- UMProject/Assets/Demo/Scripts/LaunchDemo.cs
+++ UMProject/Assets/Demo/Scripts/LaunchDemo.cs
@@ -11,31 +11,39 @@ public class LaunchDemo : MonoBehaviour
         UMOConfig.AddTable(new LanguageCfg());
         if (UMOConfig.Local != null)
         {
-            UMOConfig.Local.SwitchByType(UMOConfig.Local.GetOptions()[1].type);
+            UMOConfig.Local.SwitchByType(UMOConfig.Local.GetOptions()[0].type);
         }
+
         // UMOScene.Load("Game");
     }
 
     private void Update()
     {
+        // TestSwitchLang();
+    }
+
+    private void TestSwitchLang()
+    {
         var keyboard = Keyboard.current;
         if (keyboard == null)
             return;
-        // Debug.Log("1+");
 
         if (keyboard.digit0Key.wasPressedThisFrame)
         {
             Debug.Log("1");
             UMOConfig.Local.SwitchByType(UMOConfig.Local.GetOptions()[0].type);
         }
+
         if (keyboard.digit1Key.wasPressedThisFrame)
         {
             UMOConfig.Local.SwitchByType(UMOConfig.Local.GetOptions()[1].type);
         }
+
         if (keyboard.digit2Key.wasPressedThisFrame)
         {
             UMOConfig.Local.SwitchByType(UMOConfig.Local.GetOptions()[2].type);
         }
+
         if (keyboard.digit3Key.wasPressedThisFrame)
         {
             UMOConfig.Local.SwitchByType(UMOConfig.Local.GetOptions()[3].type);
